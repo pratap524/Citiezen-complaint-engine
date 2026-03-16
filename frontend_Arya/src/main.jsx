5252af2a578837a191988eb53227626940d8d406
pratap524 <majorpratapsinghsisodiya@gmail.com>
2026-03-16 23:36:40 +0530

    Stop global home stylesheet from leaking into other routes

--- frontend_Arya/src/main.jsx
+++ frontend_Arya/src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import AppErrorBoundary from './components/AppErrorBoundary';
-import '../styles.css';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
